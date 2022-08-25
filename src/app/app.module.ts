f3c2a124f981fc50d52004fdf00fc622a1898025
unknown <sankaralingam.psg@gmail.com>
2022-08-25 10:40:15 +0530

    updte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { HttpClientModule } from '@angular/common/http';
 
 // material
 import { MatInputModule, MatIconModule, MatCardModule, MatPaginatorModule, MatProgressBarModule } from '@angular/material';
-// sample1
+// sample1 again testing - again testing
 // adding this line for testing.. sample to sample 
-// sample2
+// sample2 again testing - again testing
 // sankar want to change this adding this line for testing..
 // videogular
 // import { VgCoreModule } from 'videogular2/core';
